c2048926f698e4049132c804a43ed515120380ad
Ahmed Dirar <engahmedderar@gmail.com>
2019-01-08 21:22:58 +0200

    Edit main_activity

---
+++ app/src/main/java/com/adirar/bookdoctor/data/Appointment.java
@@ -0,0 +1,31 @@
+package com.adirar.bookdoctor.data;
+
+public class Appointment {
+    private String id;
+    private String date;
+    private String patientId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+}
