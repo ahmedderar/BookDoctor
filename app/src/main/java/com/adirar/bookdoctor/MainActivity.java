41d352f4f1702ace672069cef3a7ce74f4d54a82
Ahmed Dirar <engahmedderar@gmail.com>
2019-01-08 20:50:50 +0200

    Add Firebase UI

--- app/src/main/java/com/adirar/bookdoctor/MainActivity.java
+++ app/src/main/java/com/adirar/bookdoctor/MainActivity.java
@@ -1,13 +1,110 @@
 package com.adirar.bookdoctor;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    public static final int RC_SIGN_IN = 1;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.text);
+
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    // User is signed in
+                    Log.i(TAG,"Already Sign In");
+                  //  intialSignIN(user.getDisplayName());
+                    Toast.makeText(MainActivity.this,
+                            "You're now signed in. Welcome",
+                            Toast.LENGTH_SHORT).show();
+
+                }else {
+                    // user is sign out
+                    List<AuthUI.IdpConfig> providers = Arrays.asList(
+                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                            new AuthUI.IdpConfig.GoogleBuilder().build());
+
+                    // Create and launch sign-in intent
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setAvailableProviders(providers)
+                                    .build(),
+                            RC_SIGN_IN);
+
+
+                }
+
+
+            }
+        };
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+           // IdpResponse response = IdpResponse.fromResultIntent(data);
+            Log.i(TAG,"Sign In Request");
+            if (resultCode == RESULT_OK) {
+                Log.i(TAG,"Logged in Successfully");
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                textView.setText(user.getDisplayName());
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                Log.i(TAG,"Faild to Login");
+                textView.setText("Wrong logging");
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"ON Resume");
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"On Pause");
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
     }
 }
